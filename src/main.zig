d289f36db5f834aeca40a76e16c1fbde9bd3c381
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-02-26 22:52:44 -0800

    resolve shit and print bytecode

--- src/main.zig
+++ src/main.zig
@@ -32,6 +32,70 @@ pub fn main() !void {
     const script = try reader.readScript(allocator);
     defer script.deinit(allocator);
 
+    std.debug.print(" --- Class Name: {s} ---\n", .{script.class_name});
+    std.debug.print(" --- Up to date script: {?} ---\n", .{script.up_to_date_script});
+    std.debug.print(" --- Super class script: {?} ---\n", .{script.super_class_script});
+    std.debug.print(" --- Modifiers: {?d} ---\n", .{script.modifiers});
+    std.debug.print("\n", .{});
+
+    // std.debug.print(" --- Type References ---\n", .{});
+    // for (script.type_references) |*type_reference| {
+    //     std.debug.print(" - Type {*} \n", .{type_reference});
+    //     if (type_reference.type_name.string) |type_name| {
+    //         std.debug.print("   - Type Name: {s}\n", .{type_name.*});
+    //     }
+    //     std.debug.print("   - Machine Type: {s}\n", .{@tagName(type_reference.machine_type)});
+    //     std.debug.print("   - Fish Type: {s}\n", .{@tagName(type_reference.fish_type)});
+    //     std.debug.print("   - Dimension Count: {d}\n", .{type_reference.dimension_count});
+    //     std.debug.print("   - Array Base Machine Type: {d}\n", .{type_reference.array_base_machine_type});
+    //     std.debug.print("   - Script: {?}\n", .{type_reference.script});
+    // }
+    // std.debug.print("\n", .{});
+
+    // std.debug.print(" --- Field References ---\n", .{});
+    // for (script.field_references) |*field_reference| {
+    //     std.debug.print(" - Field {*} \n", .{field_reference});
+    //     if (field_reference.name.string) |type_name| {
+    //         std.debug.print("   - Name: {s}\n", .{type_name.*});
+    //     }
+    //     std.debug.print("   - Type: {*}\n", .{field_reference.type_reference.type_reference});
+    // }
+    // std.debug.print("\n", .{});
+
+    std.debug.print(" --- Functions ---\n", .{});
+    for (script.functions) |*function| {
+        std.debug.print(" - Function {*} \n", .{function});
+        std.debug.print("   - Modifiers: {d}\n", .{function.modifiers});
+        std.debug.print("   - Stack size: {d}\n", .{function.stack_size});
+        if (function.name.string) |type_name| {
+            std.debug.print("   - Name: {s}\n", .{type_name.*});
+        }
+        std.debug.print("   - Type: {*}\n", .{function.type_reference.type_reference});
+
+        if (function.arguments.slice.len > 0) {
+            std.debug.print("   - Arguments\n", .{});
+            for (function.arguments.slice) |argument| {
+                std.debug.print("      - Offset: {d}\n", .{argument.offset});
+                std.debug.print("      - Type: {*}\n", .{argument.type_reference.type_reference});
+            }
+        }
+
+        if (function.bytecode.slice.len > 0) {
+            std.debug.print("   - Bytecode\n", .{});
+            for (function.bytecode.slice, function.line_numbers.slice, 0..) |bytecode, line_number, i| {
+                std.debug.print("      {d:0>4}: 0x{x:0>16} {s} (ln: {d})\n", .{
+                    i,
+                    bytecode,
+                    @tagName(try std.meta.intToEnum(
+                        Reader.InstructionType,
+                        @as(u8, @truncate(bytecode)),
+                    )),
+                    line_number,
+                });
+            }
+        }
+    }
+    std.debug.print("\n", .{});
     // std.debug.print("script class_name: {s}\n", .{script.class_name});
     // std.debug.print("{}\n", .{script});
 }
--- src/reader.zig
+++ src/reader.zig
@@ -70,15 +70,12 @@ const ResolvableTypeReference = union(enum) {
 };
 
 const ResolvableString = union(enum) {
-    string: *const []const u8,
+    string: ?*const []const u8,
     idx: u32,
 };
 
 const ResolvableFunction = union(enum) {
-    function: union(enum) {
-        function: *const Function,
-        function_definition: *const FunctionDefinition,
-    },
+    function: *const FunctionDefinition,
     idx: u32,
 };
 
@@ -258,6 +255,204 @@ pub const ResourceType = enum(u8) {
     cached_costume_data = 65,
     data_labels = 66,
     adventure_maps = 67,
+};
+
+pub const InstructionType = enum(u8) {
+    NOP = 0x0,
+    LCb = 0x1,
+    LCc = 0x2,
+    LCi = 0x3,
+    LCf = 0x4,
+    LCsw = 0x5,
+    LC_NULLsp = 0x6,
+    MOVb = 0x7,
+    LOG_NEGb = 0x8,
+    MOVc = 0x9,
+    MOVi = 0xa,
+    INCi = 0xb,
+    DECi = 0xc,
+    NEGi = 0xd,
+    BIT_NEGi = 0xe,
+    LOG_NEGi = 0xf,
+    ABSi = 0x10,
+    MOVf = 0x11,
+    NEGf = 0x12,
+    ABSf = 0x13,
+    SQRTf = 0x14,
+    SINf = 0x15,
+    COSf = 0x16,
+    TANf = 0x17,
+    MOVv4 = 0x18,
+    NEGv4 = 0x19,
+    MOVm44 = 0x1a,
+    IT_MOV_S_DEPRECATED = 0x1b,
+    MOVrp = 0x1c,
+    MOVcp = 0x1d,
+    MOVsp = 0x1e,
+    MOVo = 0x1f,
+    EQb = 0x20,
+    NEb = 0x21,
+    IT_RESERVED0_C = 0x22,
+    IT_RESERVED1_C = 0x23,
+    LTc = 0x24,
+    LTEc = 0x25,
+    GTc = 0x26,
+    GTEc = 0x27,
+    EQc = 0x28,
+    NEc = 0x29,
+    ADDi = 0x2a,
+    SUBi = 0x2b,
+    MULi = 0x2c,
+    DIVi = 0x2d,
+    MODi = 0x2e,
+    MINi = 0x2f,
+    MAXi = 0x30,
+    SLAi = 0x31,
+    SRAi = 0x32,
+    SRLi = 0x33,
+    BIT_ORi = 0x34,
+    BIT_ANDi = 0x35,
+    BIT_XORi = 0x36,
+    LTi = 0x37,
+    LTEi = 0x38,
+    GTi = 0x39,
+    GTEi = 0x3a,
+    EQi = 0x3b,
+    NEi = 0x3c,
+    ADDf = 0x3d,
+    SUBf = 0x3e,
+    MULf = 0x3f,
+    DIVf = 0x40,
+    MINf = 0x41,
+    MAXf = 0x42,
+    LTf = 0x43,
+    LTEf = 0x44,
+    GTf = 0x45,
+    GTEf = 0x46,
+    EQf = 0x47,
+    NEf = 0x48,
+    ADDv4 = 0x49,
+    SUBv4 = 0x4a,
+    MULSv4 = 0x4b,
+    DIVSv4 = 0x4c,
+    DOT4v4 = 0x4d,
+    DOT3v4 = 0x4e,
+    DOT2v4 = 0x4f,
+    CROSS3v4 = 0x50,
+    MULm44 = 0x51,
+    IT_EQ_S_DEPRECATED = 0x52,
+    IT_NE_S_DEPRECATED = 0x53,
+    EQrp = 0x54,
+    NErp = 0x55,
+    EQo = 0x56,
+    NEo = 0x57,
+    EQsp = 0x58,
+    NEsp = 0x59,
+    GET_V4_X = 0x5a,
+    GET_V4_Y = 0x5b,
+    GET_V4_Z = 0x5c,
+    GET_V4_W = 0x5d,
+    GET_V4_LEN2 = 0x5e,
+    GET_V4_LEN3 = 0x5f,
+    GET_V4_LEN4 = 0x60,
+    GET_M44_XX = 0x61,
+    GET_M44_XY = 0x62,
+    GET_M44_XZ = 0x63,
+    GET_M44_XW = 0x64,
+    GET_M44_YX = 0x65,
+    GET_M44_YY = 0x66,
+    GET_M44_YZ = 0x67,
+    GET_M44_YW = 0x68,
+    GET_M44_ZX = 0x69,
+    GET_M44_ZY = 0x6a,
+    GET_M44_ZZ = 0x6b,
+    GET_M44_ZW = 0x6c,
+    GET_M44_WX = 0x6d,
+    GET_M44_WY = 0x6e,
+    GET_M44_WZ = 0x6f,
+    GET_M44_WW = 0x70,
+    GET_M44_rX = 0x71,
+    GET_M44_rY = 0x72,
+    GET_M44_rZ = 0x73,
+    GET_M44_rW = 0x74,
+    GET_M44_cX = 0x75,
+    GET_M44_cY = 0x76,
+    GET_M44_cZ = 0x77,
+    GET_M44_cW = 0x78,
+    SET_V4_X = 0x79,
+    SET_V4_Y = 0x7a,
+    SET_V4_Z = 0x7b,
+    SET_V4_W = 0x7c,
+    SET_M44_XX = 0x7d,
+    SET_M44_XY = 0x7e,
+    SET_M44_XZ = 0x7f,
+    SET_M44_XW = 0x80,
+    SET_M44_YX = 0x81,
+    SET_M44_YY = 0x82,
+    SET_M44_YZ = 0x83,
+    SET_M44_YW = 0x84,
+    SET_M44_ZX = 0x85,
+    SET_M44_ZY = 0x86,
+    SET_M44_ZZ = 0x87,
+    SET_M44_ZW = 0x88,
+    SET_M44_WX = 0x89,
+    SET_M44_WY = 0x8a,
+    SET_M44_WZ = 0x8b,
+    SET_M44_WW = 0x8c,
+    SET_M44_rX = 0x8d,
+    SET_M44_rY = 0x8e,
+    SET_M44_rZ = 0x8f,
+    SET_M44_rW = 0x90,
+    SET_M44_cX = 0x91,
+    SET_M44_cY = 0x92,
+    SET_M44_cZ = 0x93,
+    SET_M44_cW = 0x94,
+    GET_SP_MEMBER = 0x95,
+    GET_RP_MEMBER = 0x96,
+    SET_SP_MEMBER = 0x97,
+    SET_RP_MEMBER = 0x98,
+    GET_ELEMENT = 0x99,
+    SET_ELEMENT = 0x9a,
+    GET_ARRAY_LEN = 0x9b,
+    NEW_ARRAY = 0x9c,
+    ARRAY_INSERT = 0x9d,
+    ARRAY_APPEND = 0x9e,
+    ARRAY_ERASE = 0x9f,
+    ARRAY_FIND = 0xa0,
+    ARRAY_CLEAR = 0xa1,
+    WRITE = 0xa2,
+    ARG = 0xa3,
+    CALL = 0xa4,
+    RET = 0xa5,
+    B = 0xa6,
+    BEZ = 0xa7,
+    BNEZ = 0xa8,
+    CASTsp = 0xa9,
+    INTb = 0xaa,
+    INTc = 0xab,
+    INTf = 0xac,
+    FLOATb = 0xad,
+    FLOATc = 0xae,
+    FLOATi = 0xaf,
+    BOOLc = 0xb0,
+    BOOLi = 0xb1,
+    BOOLf = 0xb2,
+    GET_OBJ_MEMBER = 0xb3,
+    SET_OBJ_MEMBER = 0xb4,
+    NEW_OBJECT = 0xb5,
+    ARRAY_RESIZE = 0xb6,
+    ARRAY_RESERVE = 0xb7,
+    LCv4 = 0xb8,
+    LC_NULLo = 0xb9,
+    CASTo = 0xba,
+    GET_SP_NATIVE_MEMBER = 0xbb,
+    LCsa = 0xbc,
+    BIT_ORb = 0xbd,
+    BIT_ANDb = 0xbe,
+    BIT_XORb = 0xbf,
+    CALLVo = 0xc0,
+    CALLVsp = 0xc1,
+    ASSERT = 0xc2,
 };
 
 pub const MachineType = enum(u8) {
@@ -498,19 +693,104 @@ pub fn MMReader(comptime Reader: type) type {
                         };
                     };
 
-                    const constant_table_s64: ?[]const u32 =
+                    const constant_table_s64: ?[]u32 =
                         if (self.revision.head >= 0x3e2)
                         try self.readArray(u32, allocator, null, ScriptReadType)
                     else
                         null;
 
-                    const constant_table_float: []const f32 = try self.readArray(f32, allocator, null, ScriptReadType);
+                    const constant_table_float: []f32 = try self.readArray(f32, allocator, null, ScriptReadType);
 
-                    const depending_guids: ?[]const u32 =
+                    const depending_guids: ?[]u32 =
                         if (self.revision.head >= 0x1ec)
                         try self.readArray(u32, allocator, null, ScriptReadType)
                     else
                         null;
+
+                    { //start resolving shit
+                        for (type_references) |*type_reference| {
+                            if (type_reference.type_name.idx != 0xFFFFFFFF)
+                                type_reference.type_name = .{ .string = &a_str_table.strings[type_reference.type_name.idx] }
+                            else
+                                type_reference.type_name = .{ .string = null };
+                        }
+
+                        for (field_references) |*field_reference| {
+                            if (field_reference.name.idx != 0xFFFFFFFF)
+                                field_reference.name = .{ .string = &a_str_table.strings[field_reference.name.idx] }
+                            else
+                                field_reference.name = .{ .string = null };
+
+                            if (field_reference.type_reference.idx != 0xFFFFFFFF)
+                                field_reference.type_reference = .{ .type_reference = &type_references[field_reference.type_reference.idx] };
+                        }
+
+                        for (function_references) |*function_reference| {
+                            if (function_reference.name.idx != 0xFFFFFFFF)
+                                function_reference.name = .{ .string = &a_str_table.strings[function_reference.name.idx] }
+                            else
+                                function_reference.name = .{ .string = null };
+
+                            if (function_reference.type_reference.idx != 0xFFFFFFFF)
+                                function_reference.type_reference = .{ .type_reference = &type_references[function_reference.type_reference.idx] };
+                        }
+
+                        for (field_definitions) |*field_definition| {
+                            if (field_definition.name.idx != 0xFFFFFFFF)
+                                field_definition.name = .{ .string = &a_str_table.strings[field_definition.name.idx] }
+                            else
+                                field_definition.name = .{ .string = null };
+
+                            if (field_definition.type_reference.idx != 0xFFFFFFFF)
+                                field_definition.type_reference = .{ .type_reference = &type_references[field_definition.type_reference.idx] };
+                        }
+
+                        for (property_definitions) |*property_definition| {
+                            if (property_definition.name.idx != 0xFFFFFFFF)
+                                property_definition.name = .{ .string = &a_str_table.strings[property_definition.name.idx] }
+                            else
+                                property_definition.name = .{ .string = null };
+
+                            if (property_definition.type_reference.idx != 0xFFFFFFFF)
+                                property_definition.type_reference = .{ .type_reference = &type_references[property_definition.type_reference.idx] };
+
+                            if (property_definition.set_function.idx != 0xFFFFFFFF)
+                                property_definition.set_function = .{ .function = &functions[property_definition.set_function.idx] };
+
+                            if (property_definition.get_function.idx != 0xFFFFFFFF)
+                                property_definition.get_function = .{ .function = &functions[property_definition.get_function.idx] };
+                        }
+
+                        for (functions) |*function| {
+                            if (function.name.idx != 0xFFFFFFFF)
+                                function.name = .{ .string = &a_str_table.strings[function.name.idx] }
+                            else
+                                function.name = .{ .string = null };
+
+                            if (function.type_reference.idx != 0xFFFFFFFF)
+                                function.type_reference = .{ .type_reference = &type_references[function.type_reference.idx] };
+
+                            function.arguments = .{ .slice = shared_arguments[function.arguments.idx.begin..function.arguments.idx.end] };
+                            function.bytecode = .{ .slice = shared_bytecode[function.bytecode.idx.begin..function.bytecode.idx.end] };
+                            function.line_numbers = .{ .slice = shared_line_numbers[function.line_numbers.idx.begin..function.line_numbers.idx.end] };
+                            function.local_variables = .{ .slice = shared_local_variables[function.local_variables.idx.begin..function.local_variables.idx.end] };
+                        }
+
+                        for (shared_arguments) |*argument| {
+                            if (argument.type_reference.idx != 0xFFFFFFFF)
+                                argument.type_reference = .{ .type_reference = &type_references[argument.type_reference.idx] };
+                        }
+
+                        for (shared_local_variables) |*local_variable| {
+                            if (local_variable.type_reference.idx != 0xFFFFFFFF)
+                                local_variable.type_reference = .{ .type_reference = &type_references[local_variable.type_reference.idx] };
+
+                            if (local_variable.name.idx != 0xFFFFFFFF)
+                                local_variable.name = .{ .string = &a_str_table.strings[local_variable.name.idx] }
+                            else
+                                local_variable.name = .{ .string = null };
+                        }
+                    }
 
                     return Script{
                         .up_to_date_script = up_to_date_script,
